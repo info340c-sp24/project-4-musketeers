24bef1b8ec7513c5b4adc26116ffb6e93cb8cc8f
Enrico Pratama <enricopratama10@gmail.com>
2024-04-20 18:57:55 -0700

    routing and pages start
    Co-authored-by: Agnes Bisma <agbsm@users.noreply.github.com>
    Co-authored-by: carolynchristo <carolynchristo@users.noreply.github.com>
    Co-authored-by: stevenwbt <stevenwbt@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/masterclass', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'masterclass.html'));
+});
+
+app.get('/explore', function(req, res) {
+
+    res.sendFile(path.join(__dirname, 'views', 'explore.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
