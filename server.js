56ff7c9d667de307aee78bf10182b72823453528
stevenwbt <stevenwilbertheng@gmail.com>
2024-04-21 21:24:08 -0700

    live server

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/masterclass', function(req, res) {
     res.sendFile(path.join(__dirname, 'views', 'masterclass.html'));
 });
 
+app.get('/viewall', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'viewall.html'));
+});
+
 app.get('/explore', function(req, res) {
     res.sendFile(path.join(__dirname, 'views', 'explore.html'));
 });
