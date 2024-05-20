6800e36ddb3a4b63742a2ce6e8aa153e858c1878
theophilaabigail <122048057+theophilaabigail@users.noreply.github.com>
2024-05-19 22:19:05 -0700

    Added firebase config.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React, { useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@material-tailwind/react';
-import Layout from './pages/Layout';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import NoPage from './pages/NoPage';
-import CommunityHub from './pages/CommunityHub';
-import MasterCards from './pages/MasterCards';
-import Masterclass from './pages/Masterclass';
-import SignUp from './pages/SignUp';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import CommunityHub from "./pages/CommunityHub";
+import MasterCards from "./pages/MasterCards";
+import Masterclass from "./pages/Masterclass";
+import SignUp from "./pages/SignUp";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHi0vaDjbtI2M9IOF0afct3adv07i1TiE",
+  authDomain: "fourmusketeers-102431.firebaseapp.com",
+  databaseURL: "https://fourmusketeers-102431-default-rtdb.firebaseio.com",
+  projectId: "fourmusketeers-102431",
+  storageBucket: "fourmusketeers-102431.appspot.com",
+  messagingSenderId: "492158954689",
+  appId: "1:492158954689:web:a92457b726ad35271a7840",
+  measurementId: "G-BJKMTWHXXZ",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 function App() {
   return (
@@ -35,7 +57,7 @@ function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
